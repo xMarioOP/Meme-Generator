5800dcc67264920580a51b24862c1bf5be40a1f0
xMarioOP <marioalejandro424@outlook.com>
2024-09-17 14:50:30 -0500

    Meme Generator

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import Header from "./components/Header"
+import Meme from "./components/Meme"
+import './App.css'
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
+
